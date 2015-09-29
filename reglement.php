dba2df9102160fb3b2e5cdc78f716d72c63ba635
jobran <jobran41@gmail.com>
2015-09-29 10:32:32 +0200

    #

--- reglement.php
+++ reglement.php
@@ -3,7 +3,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="myModalLabel">REGLEMENT JEU-CONCOURS « Le Memory du St-Bernard »</h4>
+        <h4 class="modal-title" id="myModalLabel">REGLEMENT JEU-CONCOURS « foire valais  »</h4>
       </div>
 		<div class="modal-body">
 			Lorem ipsum Dolore Excepteur pariatur dolore eu exercitation velit sit aute ut anim Excepteur dolor nulla ullamco mollit in Ut ea et ex enim mollit in tempor Duis ut commodo ut elit tempor ut dolore sed ex consectetur Excepteur ea Excepteur ea minim occaecat dolor enim reprehenderit ut dolore Excepteur ut laboris enim eiusmod mollit laboris laborum ex laboris occaecat magna exercitation irure tempor sint ea sit do dolore non cillum Duis Excepteur eiusmod dolor consequat Excepteur in exercitation Duis et veniam ut dolor aliqua magna voluptate dolore aliqua et eu ut sint exercitation dolore ut Excepteur laborum commodo adipisicing et consectetur in culpa exercitation voluptate nisi sed in consectetur irure esse esse fugiat sunt do fugiat est aute officia adipisicing deserunt laborum voluptate mollit do enim incididunt nisi consectetur minim est nostrud eiusmod voluptate sunt nisi.
